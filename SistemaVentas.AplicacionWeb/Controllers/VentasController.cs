93a6c585fc7c5f6b12ecead4cb6039db590cfccf
luis manuel lópez morales <luis_20_04_99@hotmail.com>
2023-01-11 13:34:27 -0600

    Desarrollo de la parte Frontend

---
+++ SistemaVentas.AplicacionWeb/Controllers/VentasController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaVentas.AplicacionWeb.Controllers
+{
+    public class VentasController : Controller
+    {
+        public IActionResult NuevaVenta()
+        {
+            return View();
+        }
+        public IActionResult HistorialVenta()
+        {
+            return View();
+        }
+    }
+}
---
+++ SistemaVentas.IOC/Dependencia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SistemaVentas.DAL;
+
+namespace SistemaVentas.IOC
+{
+    public static class Dependencia
+    {
+        public static void InyectarDependencia(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<SistemaVentasContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("ConectionSQL"));
+            }
+            );
+        }
+    }
+}
